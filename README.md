8c046e2bec06107526d665dfb8377bf1842963fb
Felicitas Pojtinger <felicitas@pojtinger.com>
2021-02-27 12:39:26 +0100

    feat: Add initial WIP build script implementation

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# xeus-cling-binaries
+# xeus-cling Binaries
+
 Weekly builds of https://github.com/jupyter-xeus/xeus-cling.
+
+![Docker CI](https://github.com/pojntfx/xeus-cling-binaries/workflows/Docker%20CI/badge.svg)
+
+## Installation
+
+Binaries packages (`.tar.gz`, built against the GNU `libc`) are built weekly and uploaded to [GitHub releases](https://github.com/pojntfx/xeus-cling-binaries/releases).
+
+## License
+
+xeus-cling Binaries (c) 2021 Felicitas Pojtinger and contributors
+
+SPDX-License-Identifier: BSD-3-Clause
---
+++ build.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Install build dependencies
+apt update
+apt install -y build-essential cmake curl git
+
+# Install miniforge
+XEUS_PREFIX=/usr/local/xeus-cling
+curl -L -o /tmp/miniforge.sh https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-$(uname -m).sh
+chmod +x /tmp/miniforge.sh
+mkdir -p ${XEUS_PREFIX}
+/tmp/miniforge.sh -bfp ${XEUS_PREFIX} -u
+rm /tmp/miniforge.sh
+
+# Add miniforge to the path temporarily
+export PATH="$PATH:${XEUS_PREFIX}/bin"
+
+# Install dependencies
+conda install -c conda-forge -y cmake xeus=1.0.0 cling=0.8 clangdev=5.0 llvmdev=5 nlohmann_json cppzmq xtl pugixml cxxopts
+
+# Install xeus-cling
+XEUS_CLING_VERSION=0.12.0
+rm -rf /tmp/xeus-cling
+cd /tmp
+git clone https://github.com/jupyter-xeus/xeus-cling.git
+cd xeus-cling
+git checkout ${XEUS_CLING_VERSION}
+mkdir -p build && cd build
+cmake -D CMAKE_INSTALL_PREFIX=${XEUS_PREFIX} -D CMAKE_INSTALL_LIBDIR=${XEUS_PREFIX}/lib -D DOWNLOAD_GTEST=ON ..
+make install -j$(nproc)
+rm -rf /tmp/xeus-cling
+
+# Remove build dependencies
+conda clean -y -f
+
+# Create tar archive
+cd ${XEUS_PREFIX}
+mkdir -p /out
+tar -zcvf /out/xeus-cling.$(uname -m).tar.gz *
